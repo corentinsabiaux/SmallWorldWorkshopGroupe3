d60ef6452cc32972730a7e0fb2af87ee6fef4343
Aaron <asticot.malin@gmail.com>
2020-04-28 12:01:24 +0200

    ajout du script nomad & patch ConquestPhase ligne 123

---
+++ Assets/Scripts/GameSystem/Powers/Nomad.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nomad : Power
+{
+    public Nomad()
+    {
+        name = "Nomad";
+        desc = "";
+        troopsNumber = 5;
+        turnConquest = 0;
+    }
+    public int turnConquest;
+    public override void StartTurn(Player p)
+    {
+        turnConquest = 0;
+    }
+    public override bool CanConquest(int boardPos, Player p)
+    {
+        return false;
+    }
+    public override void ConquestCost(int boardPos, ref int cost, Player p)
+    {
+        
+    }
+    public override void Conquest(int boardPos, Player p)
+    {
+        turnConquest++;
+        Debug.Log("waw");
+    }
+    public override int VictoryPointGain(Player p)
+    {
+        int gain = turnConquest * 10;
+        return gain;
+    }
+
+    public override void EndTurn(Player p)
+    {
+
+    }
+
+    public override void LoosConquestedCase(int boardPos, Player p)
+    {
+
+    }
+
+    public override void Declin(Player p)
+    {
+
+    }
+}
